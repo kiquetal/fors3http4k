3f9d702e892a2974d202357413d788f2cfe726a0
kiquetal <kiquetal@gmail.com>
2020-06-06 12:40:27 -0400

    adding simple filter to log

--- src/main/kotlin/quickstart/Server.kt
+++ src/main/kotlin/quickstart/Server.kt
@@ -1,13 +1,26 @@
 package quickstart
-import org.http4k.core.Request
-import org.http4k.core.Response
+import org.http4k.core.*
 import org.http4k.core.Status.Companion.OK
+import org.http4k.events.AutoJsonEvents
+import org.http4k.events.Event
+import org.http4k.events.EventFilters
+import org.http4k.events.then
+import org.http4k.filter.ResponseFilters
+import org.http4k.format.Jackson
 import org.http4k.server.Jetty
 import org.http4k.server.asServer
+import java.time.Clock
 
 
-
-    fun main(args:Array<String>) {
-        { request: Request -> Response(OK).body("Hello World" + request.query("name")) }.asServer(Jetty(8088)).start()
+fun main(args:Array<String>) {
+    val audit = ResponseFilters.ReportHttpTransaction { tx: HttpTransaction ->
+        logger("my call to ${tx.request.uri} returned ${tx.response.status} and took ${tx.duration.toMillis()}")
     }
+        val app = audit.then{ request: Request -> Response(OK).body("Hello World" + request.query("name")) }.asServer(Jetty(8088))
+
+
+        app.start()
+
+    }
+fun logger(message: String) = println("${Clock.systemUTC().instant()} $message")
 
